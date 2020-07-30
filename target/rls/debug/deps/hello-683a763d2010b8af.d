f1ec2055b5401f2605f97d2b93e7b2f7891fbc7f
jdoolittle126 <jdoolittle126@gmail.com>
2020-07-30 18:21:20 -0400

    learning from mr. ferris the crab

---
+++ target/rls/debug/deps/hello-683a763d2010b8af.d
@@ -0,0 +1,5 @@
+e:\Workspaces\vs-code-rust\hello\target\rls\debug\deps\hello-683a763d2010b8af.rmeta: src\main.rs
+
+e:\Workspaces\vs-code-rust\hello\target\rls\debug\deps\hello-683a763d2010b8af.d: src\main.rs
+
+src\main.rs:
---
+++ target/rls/debug/deps/hello-c45ab1c7aaaccda5.d
@@ -0,0 +1,5 @@
+e:\Workspaces\vs-code-rust\hello\target\rls\debug\deps\hello-c45ab1c7aaaccda5.rmeta: src\main.rs
+
+e:\Workspaces\vs-code-rust\hello\target\rls\debug\deps\hello-c45ab1c7aaaccda5.d: src\main.rs
+
+src\main.rs:
